aa9d01fcebac5c271293e1dda9d7dd1a8624c168
Yurii Shaklak <yurijfedor178@gmail.com>
2022-12-26 09:28:05 +0200

    add registration marcup

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
-  return (
+   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ImageBackground
+        source={require('./assets/images/photo_bg.jpg')}
+        style={styles.imageBg}>
+        <View style={styles.form}>
+          <View style={styles.userPhoto}></View>
+            <TextInput
+             style={styles.input}
+             placeholder={'Логин'}
+             placeholderTextColor={'#BDBDBD'}
+              />
+          
+           <View style={{marginVertical: 16}}>
+            <TextInput
+              style={styles.input}
+              placeholder={'Адрес электронной почты'}
+              placeholderTextColor={'#BDBDBD'}/>
+           </View>
+           <View style={ styles.pswContainer}>
+             <TextInput
+               style={styles.input} 
+            
+             placeholder={'Пароль'}
+             placeholderTextColor={'#BDBDBD'}
+               secureTextEntry={true}
+            clearButtonMode='always'
+           />
+             <Text style={ styles.pswtext}>Показать</Text>
+           </View>
+            
+        </View>
+      </ImageBackground>
+      
     </View>
   );
 }
@@ -14,7 +44,66 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  imageBg: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: 'center',
+    // alignItems: 'center'
+  },
+  form: {
+    flex: 1,
+    backgroundColor: '#FFFFFF',
+    borderTopLeftRadius: 25,
+    borderTopRightRadius: 25,
+    height: 550,
+    marginTop: 263,
+  },
+  userPhoto: {
+    width: 120,
+    height: 120,
+    backgroundColor: '#F6F6F6',
+    borderRadius: 16,
+    marginTop: -60,
+    marginHorizontal: 100 / 2
+  },
+  input: {
+    backgroundColor: '#F6F6F6',
+    borderWidth: 1, 
+    borderColor: "#E8E8E8",
+    borderTopLeftRadius: 8,
+    borderBottomLeftRadius: 8,
+    marginHorizontal: 16,
+    paddingLeft: 16,
+    height: 50,
+    fontFamily: 'Roboto',
+    fontStyles: 'normal',
+    fontWeight: '400',
+    fontSize: 16,
+    lineHeight: 1.19,
+    color: "#212121"
+  },
+  pswContainer: {
+    flexDirection: 'row',
+    width: 400
+    // flex: 1
+   
+  },
+  pswtext: {
+    flexBasis: 100,
+    paddingTop: 15,
+    backgroundColor: '#F6F6F6',
+     borderWidth: 1, 
+    borderColor: "#E8E8E8",
+    borderTopRightRadius: 8,
+    borderBottomRightRadius: 8,
+    marginLeft: -16,
+    borderLeftWidth: 0,
+    // justifyContent: 'center',
+    // alignItems: 'center'
+    // padding: 10,
+    // margin: 5,
+    // alignItems: 'center',
+  }
+  
 });
